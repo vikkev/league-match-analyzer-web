5f443080d862465d847066e76a9055ea4205cd35
Vinicius Marcel Ibaraki <vinnibaraki@gmail.com>
2026-03-14 20:56:47 -0300

    fix:errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "@/app"
+import App from "./app.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
